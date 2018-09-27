b69104f092a111a602154b2ac8911595a4b22719
Darren Zou <zoudarrenn7@gmail.com>
2018-09-27 13:27:55 -0400

    d

--- hw_03/sqrt.py
+++ hw_03/sqrt.py
@@ -1,9 +1,15 @@
 def mysqrt(int):
-    guess = 5
-    while (guess * guess != int):
-        print(guess) 
-        guess = ((guess) + (int/guess)) / 2
-    print(guess)
-      
-print(mysqrt(21))
-        
+    guess = float(5)
+    answer = 0
+    while (guess * guess != float(int)):
+        answer = ((guess) + (int/guess)) / 2
+        if guess == answer:
+            return "UNSQUARABLE BUTT Closest square root is:", guess
+        guess = answer
+        print(guess)
+        print(guess*guess)       
+    return "SqRoot is:", guess
+             
+             
+print(mysqrt(20)) 
+print(mysqrt(16))
